50fb7600adfe94fb1674239fb0b9a5454f734498
Andrew Ellis <andrewellis@pm.me>
2026-04-02 22:14:36 +0200

    Init R package skeleton with DESCRIPTION, test harness, and shared fixtures

---
+++ R/autoskill-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom rlang %||% .data := abort warn inform
+#' @importFrom cli cli_abort cli_warn cli_inform
+#' @importFrom glue glue
+#' @importFrom stats setNames
+NULL
